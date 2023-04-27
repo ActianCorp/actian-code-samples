6b34a9d2add5c061d926298a62168e6b3d0bfc6b
hab6 <115743596+hab6@users.noreply.github.com>
2023-04-27 11:08:00 -0500

    Create getcap.py

---
+++ python/getcap.py
@@ -0,0 +1,19 @@
+# Requires installation of pyodbc package prior to execution.
+
+import sys
+import pyodbc
+
+if len(sys.argv) < 2:
+    print("\nPlease provide connection URL surrounded by double-quotes. e.g.\n")
+    print("  python getcap.py \"Driver={Actian II};Server=(local);Database=anydb\"")
+    quit()
+
+url = sys.argv[1]
+cnxn = pyodbc.connect(url)
+cursor = cnxn.cursor()
+cursor.execute("select cap_capability, cap_value from iidbcapabilities")
+rs = cursor.fetchall()
+for i in range(0,len(rs)):
+    print(f"{rs[i][0].rstrip() :<25}","   ", rs[i][1].rstrip())
+cursor.close()
+cnxn.close()
